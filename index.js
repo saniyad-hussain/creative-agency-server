25bc7de1472943c16f3ffedbdcb8fe77ca3f0b02
saniyad-hussain <chowdhurysaniayd@gmail.com>
2020-10-17 13:47:26 -0400

    env update

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(fileUpload());
 
 const MongoClient = require('mongodb').MongoClient;
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zjgsq.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://creativeUser:fruCNCrm8BJn6DFs@cluster0.zjgsq.mongodb.net/creativeAgency?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 client.connect((err) => {
